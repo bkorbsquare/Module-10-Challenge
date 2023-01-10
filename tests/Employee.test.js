6ef3cf2fb32aec11a77884e29c3b5069787fb09d
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2023-01-09 18:33:34 -0600

    employee tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -11,3 +11,33 @@ test('Sets name via constructor arguments',() => {
     expect(e.name).toBe(name);
 });
 
+test('Sets id via cunstructor argument', () => {
+    const testValue = 100;
+    const e = new Employee('Boo', testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test('sets email via constructor argument', () => {
+    const testValue = 'name@email.com';
+    const e = new Employee('Boo', 0, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test('gets name via getName()', () => {
+    const testValue = 'Name';
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test('gets id via getId()', () => {
+    const testValue = '100';
+    const e = new Employee('Boo', testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test('gets email via getEmail()', () => {
+    const testValue = 'name@email.com';
+    const e = new Employee('Boo', 0, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
