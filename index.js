b60e9b9b3eb9ee5846ac337dea8144e719560960
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2023-01-12 20:57:56 -0600

    index.js

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const questions = async () => {
                 message: 'What is your ID Number?',
                 name: 'ID',
                 default: '0',
-            }
+            },
             {
                 type: 'input',
                 message: 'What is your email?',
                 name: 'email',
                 default: 'name@website.com',
-            }
+            },
             {
                 type: 'checkbox',
                 message: 'What is your role?',
@@ -36,6 +36,7 @@ const questions = async () => {
                 choices: ['Engineer', 'Intern', 'Manager'],
             },
         ])
+    
 
         if (answers.role === 'Manager') {
             const managerAns = await inquirer
@@ -55,7 +56,72 @@ const questions = async () => {
             );
             newStaffMemberData.push(newManager);
 
-            
+        } else if (answers.role === 'Engineer') {
+            const githubAns = await inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: 'What is your Github Username?',
+                        name: 'github',
+                        default: 'github.com/name',
+                    }
+                ])
+            const newEngineer = new Engineer (
+                answers.name,
+                answers.id, 
+                answers.email, 
+                githubAns.github,
+            );
+            newStaffMemberData.push(newEngineer);
+
+        } else if (answers.role === 'Intern') {
+            const internAns = await inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: 'What university did you attend?',
+                        name: 'school',
+                        default: 'school',
+                    }
+                ])
+            const newIntern = new Intern (
+                answers.name,
+                answers.id, 
+                answers.email, 
+                internAns.school,
+            );
+            newStaffMemberData.push(newIntern);
         }
 
-}
+    }; 
+
+    async function promptQuestions() {
+        await questions()
+
+        const addMemberAns = await inquirer
+            .prompt([
+                {
+                    name: 'addMember',
+                    type: 'list',
+                    choices: ['Add a new member', 'Create team'],
+                    message: 'What would you like to do next?',
+                },
+            ])
+
+            if (addMemberAns.addMember === 'Add a new member') {
+                return promptQuestions()
+            }
+            return createTeam();
+    }
+ 
+    
+        promptQuestions();
+
+        function createTeam () {
+            console.log ('new staff', newStaffMemberData)
+            fs.writeFileSync(
+                './output/index.html',
+                generateTeam(newStaffMemberData),
+                'utf-8'
+            );
+        }
